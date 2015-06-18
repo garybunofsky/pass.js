bfcf89d304c3cd4d2a74f7f4a4e3f26f7c583f70
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-18 10:25:43 -0400

    Added event listener.

--- pass.js
+++ pass.js
@@ -4,12 +4,14 @@ function getParameter(source) {
     var regex = new RegExp(regexS);
     var results = regex.exec(window.location.href);
     if (results == null)
-        return "";
+      return "";
     else
-        return results[1];
+      return results[1];
 } 
 // The function above simply parses the url. 
 // It is searching for strings that are inside getParamenter.
+document.addEventListener('DOMContentLoaded',function(){
 	document.getElementById('your-id-1').value = getParameter('source');
 	document.getElementById('your-id-2').value = getParameter('medium');
 	document.getElementById('your-id-3').value = getParameter('campaign');
+})
